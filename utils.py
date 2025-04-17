e9493be7de6a3c349356ad63c927a371dc914e4a
KxllSwxtch <dimashin9@gmail.com>
2025-04-17 14:23:25 +0900

    updates

--- utils.py
+++ utils.py
@@ -76,7 +76,7 @@ def get_customs_fees_manual(engine_volume, car_price, car_age, engine_type=1):
     }
 
     try:
-        response = requests.post(url, data=payload, headers=headers, proxies=proxies)
+        response = requests.post(url, data=payload, headers=headers)
         response.raise_for_status()
         return response.json()
     except requests.RequestException as e:
@@ -116,7 +116,7 @@ def get_customs_fees(engine_volume, car_price, car_year, car_month, engine_type=
     }
 
     try:
-        response = requests.post(url, data=payload, headers=headers, proxies=proxies)
+        response = requests.post(url, data=payload, headers=headers)
         response.raise_for_status()
         return response.json()
     except requests.RequestException as e:
